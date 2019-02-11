7e44674b7bbfa88140b6554ae51655afb74e6727
justinluzong <36277019+justinluzong@users.noreply.github.com>
2019-02-10 22:39:30 -0500

    Add files via upload

---
+++ final-exam/question3.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+$counter = 1;
+
+echo "<table border=\"1\">";
+while ( $counter <= 10 ) {
+	echo "<tr><td>Number ";
+	echo $counter;
+	echo "</td></tr>";
+	$counter = $counter + 2;
+}
+echo "</table>";	
+
+?>
